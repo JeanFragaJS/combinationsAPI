72a6eb1f302dfa9981769166d7e9492ff9fd4273
Jean Fraga <jeandevjs@gmail.com>
2022-07-29 21:47:43 -0300

    feat: add Catalog client with method getDataById

---
+++ clients/client-catalog.js
@@ -0,0 +1,54 @@
+import { RequestError, ServerError } from '../err/errors.js'
+import * as HttpUtil from '../utils/request.js'
+import { config } from '../api-get-combination-by-id/config.js'
+
+
+
+/*
+  CLIENT CATALOG IMPLEMENTATION  
+*/
+
+ export class Catalog {
+  constructor ( request = new HttpUtil.Request()) {
+    this.request = request
+    this.appKey = config.appKey  
+    this.appToken = config.appToken
+    this.accountName = config.accountName
+    this.environment = config.environment
+  
+
+  }
+
+  async  getDataById (id) {
+    try{
+      const response = await this.request.get (
+        `https://${this.accountName}.${this.environment}.com/api/catalog_system/pub/products/variations/${id}`,
+        {
+          headers: 
+          {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "X-VTEX-API-AppKey": this.appKey,
+            "X-VTEX-API-AppToken": this.appToken
+          }
+        }
+      )
+      console.log(JSON.stringify(response.data))
+      return  response.data
+
+    } catch (error) {
+      const err = this.request.isRequestError(error)
+      if ( err ) 
+        throw  new RequestError( error )
+      
+      throw new ServerError( error )
+    }
+
+  }
+
+  async normalizeData () {} // possível método privado para moldar os dados 
+}
+
+
+
+
